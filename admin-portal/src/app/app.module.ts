7cc3b5990bb69ddd47b69f7635d8b748f126ea45
swodd <swodd@mail.ru>
2018-03-22 09:54:40 +0200

    add loginService

--- admin-portal/src/app/app.module.ts
+++ admin-portal/src/app/app.module.ts
@@ -11,12 +11,16 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { LoginService } from './services/login.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
