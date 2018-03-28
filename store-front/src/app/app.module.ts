c88a05d6be4a340348db93e0be8fa24816776378
swodd <swodd@mail.ru>
2018-03-28 12:18:44 +0300

    Front: setup folder structure

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import {routing} from './app.routing';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatListModule,
+    MatDialogModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
