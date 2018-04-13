f31d002d86bafc81c6a6c37c0c455da61908f039
swodd <swodd@mail.ru>
2018-04-13 15:34:17 +0300

    add book list template

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -21,12 +21,14 @@ import { LoginService } from './services/login.service';
 import { UserService } from './services/user.service';
 import { PaymentService } from './services/payment.service';
 import { ShippingService } from './services/shipping.service';
+import { BookService } from './services/book.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MyAccountComponent } from './components/my-account/my-account.component';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 
 @NgModule({
@@ -35,7 +37,8 @@ import { MyProfileComponent } from './components/my-profile/my-profile.component
     HomeComponent,
     NavBarComponent,
     MyAccountComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
@@ -59,7 +62,8 @@ import { MyProfileComponent } from './components/my-profile/my-profile.component
     LoginService,
     UserService,
     PaymentService,
-    ShippingService
+    ShippingService,
+    BookService
   ],
   bootstrap: [AppComponent]
 })
