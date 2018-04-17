034fb082ae4329d266111a08e5afa493e318fbba
swodd <swodd@mail.ru>
2018-04-17 13:03:26 +0300

    add shopping cart template

--- store-front/src/app/app.routing.ts
+++ store-front/src/app/app.routing.ts
@@ -6,6 +6,7 @@ import {MyAccountComponent} from './components/my-account/my-account.component';
 import {MyProfileComponent} from './components/my-profile/my-profile.component';
 import {BookListComponent} from './components/book-list/book-list.component';
 import {BookDetailComponent} from './components/book-detail/book-detail.component';
+import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
 
 const appRoutes: Routes = [
   {
@@ -32,6 +33,10 @@ const appRoutes: Routes = [
   {
     path: 'bookDetail/:id',
     component: BookDetailComponent
+  },
+  {
+    path: 'shoppingCart',
+    component: ShoppingCartComponent
   }
 ];
 
--- store-front/src/app/models/cart-item.ts
+++ store-front/src/app/models/cart-item.ts
@@ -3,6 +3,7 @@ import {ShoppingCart} from './shopping-cart';
 
 export class CartItem {
 	public id: number;
+	public qty: number;
 	public subtotal: number;
 	public book: Book;
 	public shoppingCart: ShoppingCart
